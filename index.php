466524189b8573af269338540a260b0625c4106a
Klarida <klarida19@gmail.com>
2022-06-20 09:15:29 +0200

    ex8

--- index.php
+++ index.php
@@ -98,3 +98,21 @@ function copyright( $year) {
 }
 //print the copyright
 copyright((int)date('Y'));
+
+
+new_exercise(8);
+function login(string $email, string $password)
+{
+    if ($email == 'john@example.be' || $password == 'pocahontas') {
+        return 'Welcome John';
+    }
+    return 'No access';
+}
+//do not change anything below
+//should great the user with his full name (John Smith)
+echo login('john@example.be', 'pocahontas');
+//no access
+// echo login('john@example.be', 'dfgidfgdfg');
+//no access
+// echo login('wrong@example.be', 'wrong');
+//you can change things again!
